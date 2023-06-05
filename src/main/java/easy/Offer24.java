bc4c85f72d8391e53eb4a9a10d61c3cea7131262
zhuyifan10045 <zyfkilig@gmail.com>
2023-06-05 18:55:37 +0800

    question:Offer20、Offer21、Offer22、Offer24

--- src/main/java/easy/Offer24.java
+++ src/main/java/easy/Offer24.java
@@ -3,6 +3,7 @@ package easy;
 import support.Kiligz;
 import support.ListNode;
 import type.DataStructure;
+import type.TwoPointer;
 
 /**
  * 反转链表
@@ -12,7 +13,7 @@ import type.DataStructure;
  * @author Ivan
  * @since 2023/6/5
  */
-public class Offer24 implements DataStructure.Linked {
+public class Offer24 implements DataStructure.Linked, TwoPointer {
     public static void main(String[] args) {
         ListNode head = Kiligz.toListNode("1,2,3,4,5");
         Kiligz.print(new Offer24().reverseList(head));
